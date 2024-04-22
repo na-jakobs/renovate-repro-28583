23fa412aa1979ebd2f43de74d57dcac907019e1b
Jakob Sigurðsson <jakob.sigurdsson@netapp.com>
2024-04-22 14:05:14 +0000

    feat: add workflow and config

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  branchPrefix: "renovate/",
+  onboarding: false,
+  requireConfig: "required",
+  platform: "github",
+  forkProcessing: "disabled",
+  repositories: ["na-jakobs/renovate-repro-28583"],
+};
